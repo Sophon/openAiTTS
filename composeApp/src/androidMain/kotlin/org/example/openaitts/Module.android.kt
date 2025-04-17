9c29767379ee5767903cef11f10123bee29528ab
Le Duy Anh <lenduya@gmail.com>
2025-04-17 11:29:45 +0200

    fix (ktor): use CIO for Android ktor

--- composeApp/src/androidMain/kotlin/org/example/openaitts/Module.android.kt
+++ composeApp/src/androidMain/kotlin/org/example/openaitts/Module.android.kt
@@ -1,13 +1,13 @@
 package org.example.openaitts
 
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.android.Android
+import io.ktor.client.engine.cio.CIO
 import org.example.openaitts.feature.tts.domain.AudioFileManager
 import org.koin.dsl.module
 
 actual val platformModule = module {
     //core
-    single<HttpClientEngine> { Android.create() }
+    single<HttpClientEngine> { CIO.create() }
 
     //tts
     single { AudioFileManager(get()) }
