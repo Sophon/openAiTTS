d2cb26361fbe1579ee956076f8b77671e95b6c20
Le Duy Anh <lenduya@gmail.com>
2025-05-13 08:21:55 +0200

    fix (audio): change mode from communication to media
    This has better volume.

--- composeApp/src/androidMain/kotlin/org/example/openaitts/feature/audio/AudioPlayer.android.kt
+++ composeApp/src/androidMain/kotlin/org/example/openaitts/feature/audio/AudioPlayer.android.kt
@@ -9,20 +9,27 @@ import io.github.aakira.napier.Napier
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import java.io.File
+import java.io.FileOutputStream
 
 actual class AudioPlayer(private val context: Context) {
     private val audioTrack = createAudioTrack()
     private val audioChannel = Channel<ByteArray>(capacity = Channel.UNLIMITED)
     private var streamingJob: Job? = null
+    private val myData: MutableList<ByteArray> = mutableListOf()
 
     @OptIn(DelicateCoroutinesApi::class)
     actual fun cache(data: ByteArray) {
         if (audioChannel.isClosedForSend.not()) {
+            Napier.d(tag = TAG) { "caching data" }
             audioChannel.trySend(data)
+            myData.add(data)
+        } else {
+            Napier.d(tag = TAG) { "channel closed, cannot cache" }
         }
     }
 
@@ -33,6 +40,7 @@ actual class AudioPlayer(private val context: Context) {
             audioTrack.play()
 
             streamingJob = CoroutineScope(Dispatchers.IO).launch {
+                Napier.d(tag = TAG) { "writing from channel to audiotrack" }
                 for (chunk in audioChannel) {
                     audioTrack.write(chunk, 0, chunk.size)
                 }
@@ -43,16 +51,17 @@ actual class AudioPlayer(private val context: Context) {
     actual fun stop() {
         streamingJob?.cancel()
         audioTrack.apply {
-            stop()
-            release()
+            pause()
+            flush()
         }
-        audioChannel.close()
+
+        clearChannel()
     }
 
     actual fun retrieveFile(path: String): ByteArray? {
         try {
-//            return File(context.cacheDir, "snow.wav").readBytes()
-            return File(context.cacheDir, path).readBytes()
+            return File(context.cacheDir, "snow.wav").readBytes()
+//            return File(context.cacheDir, path).readBytes()
         } catch (e: Exception) {
             Napier.e(tag = TAG) { "Error: ${e.message}" }
             return null
@@ -66,7 +75,7 @@ actual class AudioPlayer(private val context: Context) {
 
         return AudioTrack(
             AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
                 .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
                 .build(),
             AudioFormat.Builder()
@@ -76,8 +85,15 @@ actual class AudioPlayer(private val context: Context) {
                 .build(),
             bufferSize,
             AudioTrack.MODE_STREAM,
-            AudioManager.AUDIO_SESSION_ID_GENERATE
+            AudioManager.AUDIO_SESSION_ID_GENERATE,
         )
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun clearChannel() {
+        while (audioChannel.isEmpty.not()) {
+            audioChannel.tryReceive().getOrNull()
+        }
     }
 }
 
